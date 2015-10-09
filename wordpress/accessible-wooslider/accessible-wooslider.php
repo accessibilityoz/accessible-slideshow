7804f1d9fd4bca20df7454ccdd72448978033eb5
Matt McLeod <matt@accessibilityoz.com.au>
2015-10-09 15:27:11 +1100

    Initial stub

---
+++ wordpress/accessible-wooslider/accessible-wooslider.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Accessible WooSlider
+Plugin URI:  https://github.com/accessibilityoz/accessible-slideshow
+Description: A small overlay for WooSlider to make it accessible
+Version:     1.0
+Author:      AccessibilityOz
+Author URI:  http://www.accessibilityoz.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: accessible-wooslider
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
